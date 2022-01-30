35d38805e17c378aac3193746181c260c0c61102
John F. Davis <davis@zatoichi.jfdhome.net>
2022-01-29 22:07:33 -0500

    mvprc1

--- webapp/mvprc1/public/js/predict.js
+++ webapp/mvprc1/public/js/predict.js
@@ -1,12 +1,16 @@
 var AWS = require("aws-sdk");
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-  }
-});
+// AWS.config.getCredentials(function(err) {
+//   if (err) console.log(err.stack);
+//   // credentials not loaded
+//   else {
+//     console.log("Access key:", AWS.config.credentials.accessKeyId);
+//   }
+// });
+
+
+console.log("Region: ", AWS.config.region);
+console.log("Access key:", AWS.config.credentials.accessKeyId);
 
 
 class MvpRc1Predict {
