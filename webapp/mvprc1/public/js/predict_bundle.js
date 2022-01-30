e1892fc542e39ee808610ca699ff32650991e452
John F. Davis <davis@zatoichi.jfdhome.net>
2022-01-29 21:28:49 -0500

    mvprc1

--- webapp/mvprc1/public/js/predict_bundle.js
+++ webapp/mvprc1/public/js/predict_bundle.js
@@ -64670,9 +64670,9 @@ class MvpRc1Predict {
             console.log("region: %s  poolId: %s  bucket: %s", this.bucketRegion, this.identityPoolId, this.albumBucketName);
 
             // a client can be shared by different commands.
-            //this.client = new AccountClient({
-            //    region: "REGION"
-            //});
+            this.client = new AccountClient({
+                region: this.bucketRegion
+            });
 
             //            AWS.config.update({
             //                region: this.bucketRegion,
