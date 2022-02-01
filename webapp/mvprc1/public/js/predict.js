2103c3017b368e8b920e1572bb17599012473492
John F. Davis <davis@zatoichi.jfdhome.net>
2022-01-31 22:09:42 -0500

    mvprc1

--- webapp/mvprc1/public/js/predict.js
+++ webapp/mvprc1/public/js/predict.js
@@ -49,137 +49,131 @@ const fetch = require('node-fetch')
 // //     }
 // //     );
 // // }
-class MvpRc1Predict {
-  // the constructor
-  constructor () {
-    console.log('MvpRc1Predict:Constructor()')
 
-    /// //////////////////////////////////////////////////////////////
-    // PREDICT radio buttons
-    /// //////////////////////////////////////////////////////////////
-    //
-    // hmm, you can't specify var or const but you can specify and assign
-    // in one step.  Then not use this.  Is this the method for specifying
-    // a class variable in javascript?
-    const TOLL147_DAVIS_DR = document.getElementById('TOLL147_DAVIS_DR')
-    const I40W_MM8 = document.getElementById('I40W_MM8')
-    const I440_US64_BYPASS = document.getElementById('I440_US64_Bypass')
-    const I26_BROADWAY = document.getElementById('I26_BROADWAY')
-    const TOLL147_HOPSON_RD = document.getElementById('TOLL147_HOPSON_RD')
-    const TOLL147_NC54 = document.getElementById('TOLL147_NC54')
-    const TOLL54_APEXBBQ = document.getElementById('TOLL54_APEXBBQ')
+/// //////////////////////////////////////////////////////////////
+// PREDICT radio buttons
+/// //////////////////////////////////////////////////////////////
+//
+// hmm, you can't specify var or const but you can specify and assign
+// in one step.  Then not use   Is this the method for specifying
+// a class variable in javascript?
+const TOLL147_DAVIS_DR = document.getElementById('TOLL147_DAVIS_DR')
+const I40W_MM8 = document.getElementById('I40W_MM8')
+const I440_US64_BYPASS = document.getElementById('I440_US64_Bypass')
+const I26_BROADWAY = document.getElementById('I26_BROADWAY')
+const TOLL147_HOPSON_RD = document.getElementById('TOLL147_HOPSON_RD')
+const TOLL147_NC54 = document.getElementById('TOLL147_NC54')
+const TOLL54_APEXBBQ = document.getElementById('TOLL54_APEXBBQ')
 
-    // I had these original as global const.  I wonder how to do this
-    // with the pedantic class member variables above?
-    const slider = document.getElementById('slider')
-    var canvas = document.getElementById('my_canvas')
-    // const predictBtn = document.getElementById('predict-btn')
+// I had these original as global const.  I wonder how to do this
+// with the pedantic class member variables above?
+const slider = document.getElementById('slider')
+var canvas = document.getElementById('my_canvas')
+// const predictBtn = document.getElementById('predict-btn')
 
-    // This was a var and not a const?
-    var ctx = canvas.getContext('2d')
+// This was a var and not a const?
+var ctx = canvas.getContext('2d')
 
-    // The camera feed img
-    // This was a var and not a const?
-    var cameraFeedImg = document.getElementsByClassName('camera_feed')[0]
+// The camera feed img
+// This was a var and not a const?
+var cameraFeedImg = document.getElementsByClassName('camera_feed')[0]
 
-    console.log(TOLL147_DAVIS_DR)
-    console.log(I40W_MM8)
-    console.log(I440_US64_BYPASS)
-    console.log(I26_BROADWAY)
-    console.log(TOLL147_HOPSON_RD)
-    console.log(TOLL147_NC54)
-    console.log(TOLL54_APEXBBQ)
-    console.log(slider)
-    console.log(canvas)
-    // console.log(predictBtn)
-    console.log(ctx)
-    console.log(cameraFeedImg)
-    console.log()
-    console.log()
-    this.addListeners()
-  }
+console.log(TOLL147_DAVIS_DR)
+console.log(I40W_MM8)
+console.log(I440_US64_BYPASS)
+console.log(I26_BROADWAY)
+console.log(TOLL147_HOPSON_RD)
+console.log(TOLL147_NC54)
+console.log(TOLL54_APEXBBQ)
+console.log(slider)
+console.log(canvas)
+// console.log(predictBtn)
+console.log(ctx)
+console.log(cameraFeedImg)
+console.log()
+console.log()
+addListeners()
 
-  // erase the canvas
-  eraseCanvas () {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-  }
-  // erase_canvas end
+// erase the canvas
+function eraseCanvas () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+// erase_canvas end
 
-  addListeners () {
     TOLL147_DAVIS_DR.addEventListener('click', button => {
-      console.log('TOLL147_DAVIS_DR: click() %o %o', button, this.TOLL147_DAVIS_DR)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_DavisDr.JPG'
-      this.eraseCanvas()
+      console.log('TOLL147_DAVIS_DR: click() %o %o', button, TOLL147_DAVIS_DR)
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_DavisDr.JPG'
+      eraseCanvas()
     }
     )
 
     I40W_MM8.addEventListener('click', button => {
       console.log('I40W_MM8: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I40W_mm8.jpg'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I40W_mm8.jpg'
+      eraseCanvas()
     }
     )
 
     I440_US64_Bypass.addEventListener('click', button => {
       console.log('I440_US64_Bypass: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I440_I87.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I440_I87.JPG'
+      eraseCanvas()
     }
     )
 
     I26_BROADWAY.addEventListener('click', button => {
       console.log('I26_BROADWAY: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I26_Broadway.jpg'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I26_Broadway.jpg'
+      eraseCanvas()
     }
     )
 
     TOLL147_HOPSON_RD.addEventListener('click', button => {
       console.log('TOLL147_HOPSON_RD: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_HopsonRd.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_HopsonRd.JPG'
+      eraseCanvas()
     }
     )
 
     TOLL147_NC54.addEventListener('click', button => {
       console.log('TOLL147_NC54: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_NC54.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll147_NC54.JPG'
+      eraseCanvas()
     }
     )
 
     TOLL54_APEXBBQ.addEventListener('click', button => {
       console.log('TOLL54_APEXBBQ: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_Apex-BBQ.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_Apex-BBQ.JPG'
+      eraseCanvas()
     }
     )
 
     TOLL540_TOLL147.addEventListener('click', button => {
       console.log('TOLL540_TOLL147: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_Toll147.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_Toll147.JPG'
+      eraseCanvas()
     }
     )
 
     TOLL540_NC55.addEventListener('click', button => {
       console.log('TOLL540_NC55: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_NC55.JPG'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_NC55.JPG'
+      eraseCanvas()
     }
     )
 
     TOLL540_MCCRIMMON.addEventListener('click', button => {
       console.log('TOLL540_MCCRIMMON: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_McCrimmonRd.jpg'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=Toll540_McCrimmonRd.jpg'
+      eraseCanvas()
     }
     )
 
     I40_DAVIS_DR.addEventListener('click', button => {
       console.log('I40_DAVIS_DR: click() %o ', button)
-      this.cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I40_DavisDr.jpg'
-      this.eraseCanvas()
+      cameraFeedImg.src = 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=I40_DavisDr.jpg'
+      eraseCanvas()
     }
     )
 
@@ -187,58 +181,53 @@ class MvpRc1Predict {
     slider.addEventListener('click', button => {
       console.log('slider: click() value is %o %o', slider.checked, button)
       if (slider.checked) {
-        this.cameraFeedImg.style.maxWidth = ''
-        this.cameraFeedImg.style.maxHeight = ''
+        cameraFeedImg.style.maxWidth = ''
+        cameraFeedImg.style.maxHeight = ''
       } else {
-        this.cameraFeedImg.style.maxWidth = '200px'
-        this.cameraFeedImg.style.maxHeight = '200px'
+        cameraFeedImg.style.maxWidth = '200px'
+        cameraFeedImg.style.maxHeight = '200px'
       }
-      this.eraseCanvas()
+      eraseCanvas()
     }
     )
     // slider end
-  }
-  // add_listeners end
 
   // json version
-  async fetchMyConfig () {
-    console.log('MyApp: getJSON()')
-    return await fetch('/myconfig').then((response) => response.json()).then((responseJson) => {
-      this.bucketRegion = responseJson.bucketRegion
-      this.identityPoolId = responseJson.identityPoolId
-      this.albumBucketName = responseJson.albumBucketName
-      console.log('region: %s  poolId: %s  bucket: %s', this.bucketRegion, this.identityPoolId, this.albumBucketName)
+async function fetchMyConfig () {
+  console.log('MyApp: getJSON()')
+  return await fetch('/myconfig').then((response) => response.json()).then((responseJson) => {
+    bucketRegion = responseJson.bucketRegion
+    identityPoolId = responseJson.identityPoolId
+    albumBucketName = responseJson.albumBucketName
+    console.log('region: %s  poolId: %s  bucket: %s', bucketRegion, identityPoolId, albumBucketName)
 
-      AWS.config.update({
-        region: this.bucketRegion,
-        credentials: new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: this.identityPoolId
-        })
+    AWS.config.update({
+      region: bucketRegion,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: identityPoolId
       })
+    })
 
-      this.s3 = new AWS.S3({
-        apiVersion: '2006-03-01',
-        params: {
-          Bucket: this.albumBucketName
-        }
-      })
+    s3 = new AWS.S3({
+      apiVersion: '2006-03-01',
+      params: {
+        Bucket: albumBucketName
+      }
+    })
 
-      console.log('Region: ', AWS.config.region)
+    console.log('Region: ', AWS.config.region)
 
-      return responseJson
-    }
-    ) // then END
+    return responseJson
   }
-
-  async doMyInit () {
-    console.log('MyApp: do_init()')
-
-    var msg = await this.fetchMyConfig()
-    console.log(msg)
-  }
-  // do_my)init END
+  ) // then END
 }
 
-// Build class with some of these specified?
-const mvprc1predict = new MvpRc1Predict()
-mvprc1predict.doMyInit()
+async function doMyInit () {
+  console.log('MyApp: do_init()')
+
+  var msg = await fetchMyConfig()
+  console.log(msg)
+}
+// do_my)init END
+
+doMyInit()
