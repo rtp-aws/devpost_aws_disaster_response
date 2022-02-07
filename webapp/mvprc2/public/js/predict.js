4f5ab91782ad9cc1c2c31eb620f328baf8e20c8d
John F. Davis <davis@zatoichi.jfdhome.net>
2022-02-07 13:59:44 -0500

    mvprc2

--- webapp/mvprc2/public/js/predict.js
+++ webapp/mvprc2/public/js/predict.js
@@ -83,7 +83,7 @@ function uploadCallback(err, data) {
             }
         },
         MaxLabels: 100,
-        MinConfidence: 70
+        MinConfidence: 10
     };
     
     performRekognition(params, data)
